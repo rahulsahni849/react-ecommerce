0701808d75faa380991a4dad71f8d9670db750a6
kashyapjain <kashyapjain2018@gmail.com>
2022-07-26 17:05:48 +0530

    created backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+//const cors = require("cors");
 
+const app = express();
+
+dotenv.config();
+
+const connectionstring = process.env.MONGO_URL;
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.listen(process.env.PORT, () => { console.log("Server Started"); });
+
+mongoose.connect(connectionstring)
+    .then(() => { console.log("Database Connected") })
+    .catch((err) => { console.log(err) });
