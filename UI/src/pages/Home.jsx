102a43bc9c95aa104de534d9d3f26492b073cb04
Rahul Sahni <rahulsahni849@gmail.com>
2022-07-29 12:34:46 +0530

    again added productitem.jsx

--- UI/src/pages/Home.jsx
+++ UI/src/pages/Home.jsx
@@ -1,10 +1,13 @@
-<<<<<<< HEAD
+import styled from "styled-components";
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import ProductsList from "../components/ProductsList.jsx";
 import Slider from "../components/Slider";
 import Announcements from "../components/Announcements";
-import styled from "styled-components";
-import ProductsList from "../components/ProductsList";
-import React from "react";
 import Categories from "../components/Categories";
+
+const Container = styled.div``;
 
 const HomeContainer = styled.div`
   display: flex;
@@ -13,50 +16,20 @@ const HomeContainer = styled.div`
 `;
 const Home = () => {
   return (
-    <HomeContainer>
+    <Container>
+      <Navbar />
+
       <Announcements />
-      <Slider />
-      <Categories />
-      <ProductsList />
-    </HomeContainer>
+
+      <HomeContainer>
+        <Slider />
+        <Categories />
+        <ProductsList />
+      </HomeContainer>
+
+      <Footer />
+    </Container>
   );
 };
 
 export default Home;
-=======
-import styled from "styled-components";
-import React from "react";
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import ProductsList from "../components/ProductsList.jsx";
-import Slider from '../components/Slider';
-import Announcements from "../components/Announcements";
-import Categories from "../components/Categories";
-
-const Container = styled.div``
-
-const HomeContainer = styled.div`
-    display:flex;
-    flex-direction: column;
-    background-color: #EEEEEE;
-`
-const Home = () => {
-    return (
-        <Container>
-            <Navbar />
-
-            <Announcements />
-
-            <HomeContainer>
-                <Slider />
-                <Categories />
-                <ProductsList />
-            </HomeContainer>
-
-            <Footer />
-        </Container>
-    )
-};
-
-export default Home;
->>>>>>> af6175d9b28585619a064b1a34d2cf5ceae058dc
